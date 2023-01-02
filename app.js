123e4bf9ac7ca5b3e175b12f3768619d1485cfcc
chithranjan kumar <kumarchithranjan@gmail.com>
2023-01-02 20:31:18 +0530

    using body-parser and filtering response based on get or post method

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('1st middleware', req.originalUrl);
-  next();  // executes next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="title"><input type="text" name="price" placeholder="price"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
-  console.log('2nd middleware');
-  res.send('<h1>Hello World</h1>');
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello From Express</h1>');
 })
 
 app.listen(3000);
