153c6b4f38ce4fbc5f2c7c9432e29d33688746cf
chithranjan kumar <kumarchithranjan@gmail.com>
2023-01-17 20:50:14 +0530

    creating relation between user and product table

--- connecting-to-mysql/app.js
+++ connecting-to-mysql/app.js
@@ -8,20 +8,34 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
-const app = express();
-
-app.use(cors());
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const userRoute = require('./routes/user');
 const expenseRoute = require('./routes/expense');
 
+const User = require('./models/user');
+const Product = require('./models/product');
+
+const app = express();
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use('/user', userRoute);
@@ -30,10 +44,22 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
   .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
